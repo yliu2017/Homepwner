4665701f5e9a781e0460e43a66025533c665ea4b
Tom Mac <yliu@wils326-vmxp-sl.highpoint.edu>
2017-03-15 11:56:10 -0400

    212

---
+++ Homepwner1/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Homepwner1
+//
+//  Created by Tom Liu's Mac on 3/15/17.
+//  Copyright © 2017 Yingqi Liu. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell{
+    
+
+}
