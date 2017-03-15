af911fe9440f4eb7f75a27c012496ab6767a6f73
Tom Mac <yliu@wils326-vmxp-sl.highpoint.edu>
2017-03-15 13:43:30 -0400

    Chapter 12

--- Homepwner1/ItemCell.swift
+++ Homepwner1/ItemCell.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class ItemCell: UITableViewCell{
+class ItemCell: UITableViewCell {
     
-
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
 }
--- Homepwner1/ItemsViewController.swift
+++ Homepwner1/ItemsViewController.swift
@@ -49,37 +49,17 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //Get a new or recycled cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
         
         //Set the text on the cell with the decription of the item
         //that is at the nth index of items, where n = row this cell
         //will appear in on the tableView
         let item = itemStore.allItems[indexPath.row]
         
-        let title = "Delete\(item.name)?"
-        let message = "Are you sure you want to delete this item?"
-        
-        let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        ac.addAction(cancelAction)
-        
-        let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: {(action)-> Void in
-            //Remove the item from the store
-            self.itemStore.removeItem(item)
-            
-            //Also remove that row from the tableiew with an animation
-            self.tableView.deleteRows(at: [indexPath], with: .automatic)
-            
-        })
-        ac.addAction(deleteAction)
-        
-        //Present the alert controller
-        present(ac, animated: true, completion: nil)
-        
-        
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        //Configure the cell with the Item
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel?.text = "$\(item.valueInDollars)"
 
         
         return cell
@@ -90,11 +70,27 @@ class ItemsViewController: UITableViewController {
         // if hte table view is asking to commit a delete command
         if editingStyle == .delete {
             let item = itemStore.allItems[indexPath.row]
-            //Remocw the item from the store
-            itemStore.removeItem(item)
             
-            //Also remove that row from the table view with an animation
-            tableView.deleteRows(at: [indexPath],with: .automatic)
+            let title = "Delete\(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: {(action)-> Void in
+                //Remove the item from the store
+                self.itemStore.removeItem(item)
+                
+                //Also remove that row from the tableiew with an animation
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+                
+            })
+            ac.addAction(deleteAction)
+            
+            //Present the alert controller
+            present(ac, animated: true, completion: nil)
         }
     }
     
@@ -114,6 +110,9 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left:0 , bottom: 0, right:0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
         
     }
     
