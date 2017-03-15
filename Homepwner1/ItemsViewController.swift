2cc3124f52ce8ba17a5577f1d4c4d8d5f2e768e0
Tom Mac <yliu@wils326-vmxp-sl.highpoint.edu>
2017-03-15 11:44:28 -0400

    Chapter 11

--- Homepwner1/ItemsViewController.swift
+++ Homepwner1/ItemsViewController.swift
@@ -56,8 +56,31 @@ class ItemsViewController: UITableViewController {
         //will appear in on the tableView
         let item = itemStore.allItems[indexPath.row]
         
+        let title = "Delete\(item.name)?"
+        let message = "Are you sure you want to delete this item?"
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        ac.addAction(cancelAction)
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: {(action)-> Void in
+            //Remove the item from the store
+            self.itemStore.removeItem(item)
+            
+            //Also remove that row from the tableiew with an animation
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+        })
+        ac.addAction(deleteAction)
+        
+        //Present the alert controller
+        present(ac, animated: true, completion: nil)
+        
+        
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+
         
         return cell
     }
